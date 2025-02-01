1cfda4a66631021ce65b65d63d245435e3855a01
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-02-01 12:35:11 +0530

    new file js task 20

---
+++ js_task_es-6.js
@@ -0,0 +1,23 @@
+
+
+//---- Maximum number using CAB methods....-----//
+
+////---------using call-------///
+
+let arr=[10,20,30,15,5]
+// console.log(Math.max.call(...arr))
+console.log(Math.max.call(null,...arr))
+// console.log(Math.max(...arr))
+
+//-----using apply -------------///
+
+let arr1=[10,20,30,15,45]
+console.log(Math.max.apply(null,[...arr1]))
+console.log(Math.max.apply(null,[...arr1,66,4,56,78,66]))
+
+//---------using bind -----------///
+
+let arr2=[22,11,4,55,6,5,4,45,66]
+// let binds=Math.max.bind(null,...arr2)
+let binds=Math.max.bind(...arr2)
+console.log(binds())
